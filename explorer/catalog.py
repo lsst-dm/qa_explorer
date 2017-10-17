026854d52cabbe3b35d81c0ba327d99e727639a8
Timothy Morton <tdm@astro.princeton.edu>
2017-10-17 14:25:26 -0400

    remove index matching

--- explorer/catalog.py
+++ explorer/catalog.py
@@ -123,7 +123,7 @@ class MatchedCatalog(Catalog):
         # Return columns in row-matched order
         df1 = self.cat1.get_columns(*args, **kwargs).loc[self.match_inds1]
         df2 = self.cat2.get_columns(*args, **kwargs).loc[self.match_inds2]
-        df2.set_index(dd.Series(df1.index, meta=df1.dtype))
+        # df2.set_index(dd.Series(df1.index))
 
         return df1, df2
 

77fb49e50074a36ce3b6bcda72b293f9b6ad255a
Timothy Morton <tdm@astro.princeton.edu>
2017-11-07 12:42:27 -0500

    replaced accidentally deleted line

--- explorer/catalog.py
+++ explorer/catalog.py
@@ -142,6 +142,7 @@ class MatchedCatalog(Catalog):
         return df1, df2
 
 
+    def _apply_func(self, func, query=None, how='difference'):
         df1, df2 = self.get_columns(func.columns, query=query)
         if func.allow_difference:
             id1, id2 = self.match_inds

4a83e319f847a2ee46f830165218525fab0271e6
Timothy Morton <tdm@astro.princeton.edu>
2017-11-06 23:41:36 -0500

    dropna for distances

--- explorer/catalog.py
+++ explorer/catalog.py
@@ -223,7 +223,7 @@ class MultiMatchedCatalog(MatchedCatalog):
             coadd = pd.Series(index=self.coadd_cat.index)
             aligned_dists = [coadd.align(c.match_distance)[1] for c in self.subcats]
             dist_df = pd.concat(aligned_dists, axis=1)
-            self._match_distance_df = dist_df
+            self._match_distance_df = dist_df.dropna(how='all')
             self._match_distance = dist_df.mean(axis=1)
         return self._match_distance
 

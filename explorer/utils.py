e913c1bccdb5a64e18b89c1a9da35ac0d19bef04
Timothy Morton <tdm@astro.princeton.edu>
2017-11-07 13:27:53 -0500

    pass list of tracts to get_visits

--- explorer/utils.py
+++ explorer/utils.py
@@ -1,12 +1,17 @@
 import sqlite3
 import os
 
-def get_visits(field, tract, filt, sqlitedir='/scratch/hchiang2/parejko/'):
+def get_visits(field, tracts, filt, sqlitedir='/scratch/hchiang2/parejko/'):
+    if not isinstance(tracts, list) or isinstace(tracts, tuple):
+        tracts = [tracts]
+
+    visits = []
     with sqlite3.connect(os.path.join(sqlitedir, 'db{}.sqlite3'.format(field))) as conn:
-        cursor = conn.cursor()
-        cmd = "select distinct visit from calexp where tract=:tract and filter=:filt"
-        cursor.execute(cmd, dict(tract=tract, filt=filt))
-        result = cursor.fetchall()
-    return [x[0] for x in result]
-    # return '^'.join(str(x[0]) for x in result)
+        for t in tracts:
+            cursor = conn.cursor()
+            cmd = "select distinct visit from calexp where tract=:tract and filter=:filt"
+            cursor.execute(cmd, dict(tract=t, filt=filt))
+            result = cursor.fetchall()
+            visits.append([x[0] for x in result])
+    return list(set([x for y in visits for x in y]))
 

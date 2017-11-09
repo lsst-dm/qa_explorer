02f89c683d88da204ec9c4ddc03b9f08538e18a5
Timothy Morton <tdm@astro.princeton.edu>
2017-11-09 14:44:53 -0500

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='explorer',
+      version='0.1',
+      description='Interactive QA explorer',
+      url='http://github.com/timothydmorton/qa_explorer',
+      author='Timothy D Morton',
+      license='MIT',
+      packages=['explorer'],
+      zip_safe=False)

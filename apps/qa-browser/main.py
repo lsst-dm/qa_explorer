fd3737ff269ace82d3725d9c440974bf919c6ba3
Timothy Morton <tdm@astro.princeton.edu>
2018-01-10 10:38:41 -0500

    added first section and lots of stuff

--- apps/qa-browser/main.py
+++ apps/qa-browser/main.py
@@ -1,3 +1,4 @@
+import yaml
 import numpy as np
 import holoviews as hv
 
@@ -8,8 +9,6 @@ from bokeh.layouts import layout
 from bokeh.models import Slider, Button, TextInput
 from bokeh.models.widgets import Panel, Tabs
 
-import holoviews as hv
-
 from explorer.static import filter_layout_dmap_coadd
 from explorer.static import description_layout_dmap_visit
 
@@ -19,47 +18,49 @@ rerun44 = '/project/tmorton/DM-12873/w44'
 rerun46 = '/project/tmorton/DM-12873/w46'
 
 butler44 = Butler(rerun44)
-butler46 = Butler(rerun46)
+# butler46 = Butler(rerun46)
 
 renderer = hv.renderer('bokeh').instance(mode='server')
 
-descriptions1 = ['mag_modelfit_CModel', 'mag_base_GaussianFlux', 'mag_ext_photometryKron_KronFlux']
-descriptions2 = ['e1ResidsHsm', 'e2ResidsHsm']
+config = yaml.read('config.yaml')
 
 # stream = hv.streams.Stream.define('Butler', butler=butler44)()
 
-dmap1 = filter_layout_dmap_coadd(butler=butler44, descriptions=descriptions1, scale=1.)
-dmap2 = filter_layout_dmap_coadd(butler=butler44, descriptions=descriptions2, 
-                                 styles=['psfMagHist'], scale=1.)
+def get_kwargs(section, category, default_styles=['psfMagHist', 'sky-stars', 'sky-gals']):
+    d = config[section][category]
+    descriptions = d['descriptions']
+    styles = default_styles if 'styles' not in d else d['styles']
+    return {'descriptions' : descriptions, 'styles' : styles}
+
+def get_object_dmaps(butler):
+    return [filter_layout_dmap_coadd(butler=butler44, **get_kwargs('object',cat))
+                 for cat in config['object'].keys()]
+
+object_dmaps = get_object_dmaps(butler44)
 
 def modify_doc(doc):
-    # Create HoloViews plot and attach the document
-    hvplot1 = renderer.get_widget(dmap1, None, doc)
-    hvplot2 = renderer.get_widget(dmap2, None, doc)
-
     repo_box = TextInput(value='/project/tmorton/DM-12873/w44', title='rerun',
                          css_classes=['customTextInput'])
 
+    # Create HoloViews plot and attach the document
+    object_hvplots = [renderer.get_widget(dmap, None, doc) for dmap in object_dmaps]
+
     def update_repo(attr, old, new):
         butler = Butler(new)
-        dmap = filter_layout_dmap_coadd(butler=butler, descriptions=descriptions)
-        new_plot1 = renderer.get_widget(dmap1, None, doc)
-        new_plot2 = renderer.get_widget(dmap2, None, doc)
-        plot1.children[0] = new_plot1.state
-        plot2.children[0] = new_plot2.state
+        object_dmaps = get_object_dmaps(butler=butler)
+        new_object_plots = [renderer.get_widget(dmap, None, doc) for dmap in object_dmaps]
+        for plot,new_plot in zip(object_plots, new_object_plots):
+            plot.children[0] = new_plot.state
 
     repo_box.on_change('value', update_repo)
 
-    plot1 = layout([hvplot1.state], sizing_mode='fixed')
-    plot2 = layout([hvplot2.state], sizing_mode='fixed')
-
-    tab1 = Panel(child=plot1, title='tab1')
-    tab2 = Panel(child=plot2, title='tab2')
-    tabs = Tabs(tabs=[tab1, tab2])
+    object_plots = [layout([hvplot.state], sizing_mode='fixed') for hvplot in object_hvplots]
+    object_tabs = Tabs(tabs=[Panel(child=plot, title=name) 
+                            for plot,name in zip(object_plots, config['object'].keys())])
 
     doc.add_root(repo_box)
     # doc.add_root(plot)
-    doc.add_root(tabs)
+    doc.add_root(object_tabs)
     return doc
 
 

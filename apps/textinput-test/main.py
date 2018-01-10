4e3f7b6dc10adccdf0f7bbede6355c22eafe5d13
Timothy Morton <tdm@astro.princeton.edu>
2018-01-10 13:39:57 -0500

    make butler, just because

--- apps/textinput-test/main.py
+++ apps/textinput-test/main.py
@@ -1,6 +1,13 @@
 from bokeh.io import curdoc
 from bokeh.models import TextInput
 
+from lsst.daf.persistence import Butler
+
+rerun44 = '/project/tmorton/DM-12873/w44'
+rerun46 = '/project/tmorton/DM-12873/w46'
+
+butler44 = Butler(rerun44)
+
 def modify_doc(doc):
     text_box = TextInput(value='this text is longer than 300px', title='input',
                          css_classes=['customTextInput'])

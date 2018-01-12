38a904aa91d7cc76fc7f5c61319cd0645a3f8480
Timothy Morton <tdm@astro.princeton.edu>
2018-01-11 23:13:13 -0500

    typo

--- apps/qa-browser/main.py
+++ apps/qa-browser/main.py
@@ -33,7 +33,7 @@ renderer = hv.renderer('bokeh').instance(mode='server')
 
 # stream = hv.streams.Stream.define('Butler', butler=butler44)()
 
-def get_kwargs(section, category, default_styles=['psfMagHist', 'sky-stars', 'sky-gals']
+def get_kwargs(section, category, default_styles=['psfMagHist', 'sky-stars', 'sky-gals'],
                 scale=1.):
     d = config[category]
     descriptions = d['descriptions']

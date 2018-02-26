6965ce37aae8126a4fe0b08f50db383c02ee212c
Timothy Morton <tdm@astro.princeton.edu>
2018-02-26 13:12:37 -0500

    create visitMatch notebook as well

--- scripts/generateQANotebook.py
+++ scripts/generateQANotebook.py
@@ -10,10 +10,12 @@ parser.add_argument('--output', '-o', default='QA', help='output folder')
 
 args = parser.parse_args()
 
-from explorer.notebook import Coadd_QANotebook
+from explorer.notebook import Coadd_QANotebook, VisitMatch_QANotebook
 
 coadd_nb = Coadd_QANotebook(args.repo, args.tract, args.filt)
 if not os.path.exists(args.output):
     os.makedirs(args.output)
 coadd_nb.write(os.path.join(args.output, 'coadd_{}_{}.ipynb'.format(args.tract, args.filt)))
 
+matched_nb = VisitMatch_QANotebook(args.repo, args.tract, args.filt)
+matched_nb.write(os.path.join(args.output, 'visitMatch_{}_{}.ipynb'.format(args.tract, args.filt)))

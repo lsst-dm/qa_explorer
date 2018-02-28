73d06ba73b3af054fced7fd46e5cb9a9632514b2
Timothy Morton <tdm@astro.princeton.edu>
2018-02-28 01:04:50 -0500

    change default folder name

--- scripts/generateQANotebook.py
+++ scripts/generateQANotebook.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('repo', help='data repository')
 parser.add_argument('--tract', type=int)
 parser.add_argument('--filt', type=str)
-parser.add_argument('--output', '-o', default='QA', help='output folder')
+parser.add_argument('--output', '-o', default='QA-notebooks', help='output folder')
 
 args = parser.parse_args()
 

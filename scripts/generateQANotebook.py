a7d22323c63c600fd8b6b0e475b0c2bb1c3f9ecb
Timothy Morton <tdm@astro.princeton.edu>
2018-03-02 22:26:26 -0500

    take filter out of color plot name

--- scripts/generateQANotebook.py
+++ scripts/generateQANotebook.py
@@ -13,13 +13,14 @@ args = parser.parse_args()
 
 from explorer.notebook import Coadd_QANotebook, VisitMatch_QANotebook, ColorColor_QANotebook
 
-coadd_nb = Coadd_QANotebook(args.repo, args.tract, args.filt)
 if not os.path.exists(args.output):
     os.makedirs(args.output)
+
+coadd_nb = Coadd_QANotebook(args.repo, args.tract, args.filt)
 coadd_nb.write(os.path.join(args.output, 'coadd_{}_{}.ipynb'.format(args.tract, args.filt)))
 
 matched_nb = VisitMatch_QANotebook(args.repo, args.tract, args.filt)
 matched_nb.write(os.path.join(args.output, 'visitMatch_{}_{}.ipynb'.format(args.tract, args.filt)))
 
 color_nb = ColorColor_QANotebook(args.repo, args.tract)
-color_nb.write(os.path.join(args.output, 'color_{}_{}.ipynb'.format(args.tract, args.filt)))
+color_nb.write(os.path.join(args.output, 'color_{}.ipynb'.format(args.tract)))

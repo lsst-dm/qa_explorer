23fed10467e52de128a281caa88a2a1a32f1d9c8
Timothy Morton <tdm@astro.princeton.edu>
2018-03-16 13:43:12 -0400

    create task to write merged forced_src tables to parquet
    This new CmdLineTask builds on MergeSourcesTask to merge
    deepCoadd_forced_src, deepCoadd_meas, and deepCoadd_ref
    tables from multiple filters into single
    joined parquet tables, one per patch.  The output dataset is
    deepCoadd_object, and gets loaded as a pandas dataframe
    with a multi-level column index, such that you can retrieve
    all the columns of a single table with, e.g., df['meas']['HSC-R'].

---
+++ bin.src/writeObjectTable.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from lsst.qa.explorer.writeParquet import WriteObjectTableTask
+WriteObjectTableTask.parseAndRun()
+
---
+++ python/lsst/qa/explorer/table.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import fastparquet
+from astropy.table import Table
+
+class ParquetTable(object):
+    """Shim to use butler to write pandas dataframe to parquet file
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+
+    """
+    def __init__(self, df, engine='pyarrow'):
+        self.df = df
+        self.engine = engine
+
+    def writeFits(self, filename):
+        """Write dataframe to parquet (not FITS).
+        """
+        if self.engine=='pyarrow':
+            table = pa.Table.from_pandas(self.df)
+            pq.write_table(table, filename, compression='none')
+        elif self.engine=='fastparquet':
+            fastparquet.write(filename, self.df)
+
+    @classmethod
+    def readFits(cls, filename):
+        """Read parquet file (not FITS) into pandas DataFrame.
+        """
+        return pd.read_parquet(filename, engine='pyarrow')

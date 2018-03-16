cc917228f8a8a936f50585336deb332821d0d1d9
Timothy Morton <tdm@astro.princeton.edu>
2018-03-16 13:43:12 -0400

    create task to write merged forced_src tables to parquet
    This new CmdLineTask builds on MergeSourcesTask to merge
    deepCoadd_forced_src tables from multiple filters into single
    joined parquet tables (prefixed by "filter_tag(filter)"; that is,
    HSC-G -> hscg, etc.), one per patch.

---
+++ python/lsst/qa/explorer/writeParquet.py
@@ -0,0 +1,59 @@
+from lsst.daf.persistence.butler import Butler
+from lsst.pex.config import (Config, Field, ConfigField, ListField, DictField, ConfigDictField,
+                             ConfigurableField)
+from lsst.pipe.base import Task, CmdLineTask, ArgumentParser, TaskRunner, TaskError
+from lsst.coadd.utils import TractDataIdContainer
+from lsst.pipe.tasks.multiBand import MergeSourcesTask, MergeSourcesConfig
+from lsst.pipe.tasks.multiBand import _makeGetSchemaCatalogs
+
+import functools
+import re
+
+from .table import ParquetTable
+
+
+def filter_tag(filt):
+    return re.sub('[^a-zA-Z0-9_]+', '', filt).lower()
+
+class WriteObjectTableConfig(MergeSourcesConfig):
+    priorityList = ListField(dtype=str, default=['HSC-G', 'HSC-R', 'HSC-I', 'HSC-Z', 'HSC-Y'],
+                             doc="Priority-ordered list of bands for the merge.")    
+
+class WriteObjectTableTask(MergeSourcesTask):
+    """Convert all source tables to parquet format
+    """
+    _DefaultName = "writeObjectTable"
+    ConfigClass = WriteObjectTableConfig
+
+    inputDataset = 'forced_src'
+    outputDataset = 'obj'
+    getSchemaCatalogs = lambda x : {}
+
+    def mergeCatalogs(self, catalogs, patchRef):
+        """Merge multiple catalogs.
+
+        Parameters
+        ----------
+        catalogs : dict
+            Mapping from filter names to catalogs.
+
+        Returns
+        -------
+        catalog : lsst.qa.explorer.table.ParquetTable
+            Merged dataframe, with each column prefixed by
+            `filter_tag(filt)`, wrapped in the parquet writer shim class.
+        """
+
+        dfs = []
+        for filt, table in catalogs.items():
+
+            # Convert afwTable to pandas DataFrame
+            df = table.asAstropy().to_pandas().set_index('id', drop=True)
+
+            # Add filter tag name to every column
+            tag = filter_tag(filt)
+            df = df.rename(columns={c : tag + '_' + c for c in df.columns})
+            dfs.append(df)
+
+        catalog = functools.reduce(lambda d1,d2 : d1.join(d2), dfs)
+        return ParquetTable(catalog)

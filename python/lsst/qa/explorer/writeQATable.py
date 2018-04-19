daf9c9bd8c742aac4cd9af95098e62052b07f29d
Timothy Morton <tdm@astro.princeton.edu>
2018-04-19 10:02:41 -0400

    make PostprocessCatalogTask base class

--- python/lsst/qa/explorer/writeQATable.py
+++ python/lsst/qa/explorer/writeQATable.py
@@ -23,10 +23,61 @@ from .functors import CompositeFunctor, RAColumn, DecColumn
 # Question: is there a way that LSST packages store data files?
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
+class PostprocessCatalogTask(CmdLineTask):
+    """Base class for postprocessing calculations on catalogs
+
+    """    
+
+    inputDataset = 'deepCoadd_obj'
+
+    @classmethod
+    def _makeArgumentParser(cls):
+        """Create a suitable ArgumentParser.
+        
+        """
+        parser = ArgumentParser(name=cls._DefaultName)
+        parser.add_id_argument("--id", cls.inputDataset,
+                               ContainerClass=ExistingCoaddDataIdContainer,
+                               help="data ID, e.g. --id tract=12345 patch=1,2")
+        return parser
+
+
+    def run(self, patchRef):
+        """Do calculations; write result
+        """
+        df = self.doCalculations(patchRef)
+        self.write(df, patchRef)
+        return df
+
+    def doCalculations(self, dataRef):
+        """Do postprocessing calculations
+
+        Takes a dataRef pointing to deepCoadd_obj; 
+        returns a dataframe with results of postprocessing calculations.
+
+        Parameters
+        ----------
+        dataRef : data reference
+
+        Return
+        ------
+        df : `pandas.DataFrame`
+
+        """
+        raise NotImplementedError('Subclasses must implement doCalculations')
+
+    def write(self, df, parqRef):
+        parqRef.put(ParquetTable(dataFrame=df), self.outputDataset)
+
+    def writeMetadata(self, dataRef):
+        """No metadata to write.
+        """
+        pass
+
 class WriteQATableConfig(Config):
     coaddName = Field(dtype=str, default="deep", doc="Name of coadd")
 
-class WriteQATableTask(CmdLineTask):
+class WriteQATableTask(PostprocessCatalogTask):
     """Compute columns of QA interest from coadd object tables
 
     This computes columns based on a YAML specification file,
@@ -39,41 +90,19 @@ class WriteQATableTask(CmdLineTask):
     inputDataset = 'deepCoadd_obj'
     outputDataset = 'deepCoadd_qa'
 
-    @classmethod
-    def _makeArgumentParser(cls):
-        """Create a suitable ArgumentParser.
-
-        We will use the ArgumentParser to get a list of data
-        references for patches; the RunnerClass will sort them into lists
-        of data references for the same patch. 
-
-        References first of self.inputDatasets, rather than
-        self.inputDataset (which parent class does.)
-        """
-        parser = ArgumentParser(name=cls._DefaultName)
-        parser.add_id_argument("--id", cls.inputDataset,
-                               ContainerClass=ExistingCoaddDataIdContainer,
-                               help="data ID, e.g. --id tract=12345 patch=1,2")
-        return parser
-
-
-    def run(self, dataRef):
-        parq = dataRef.get()
+    def doCalculations(self, parqRef):
+        parq = parqRef.get()
         funcs = CompositeFunctor.from_yaml(os.path.join(ROOT, 'functors.yaml'))
         funcs.funcDict.update({'ra':RAColumn(), 'dec':DecColumn()})
         dfDict = {}
         for filt in parq.columnLevelNames['filter']:
             catalog = MultilevelParquetCatalog(parq, filt=filt)
             df = funcs(catalog)
-            df['patchId'] = dataRef.dataId['patch']
+            df['patchId'] = parqRef.dataId['patch']
             dfDict[filt] = df
 
         # This makes a multilevel column index, with filter as first level
         df = pd.concat(dfDict, axis=1, names=['filter', 'column'])
-        dataRef.put(ParquetTable(dataFrame=df), self.outputDataset)
+        return df
 
 
-    def writeMetadata(self, dataRef):
-        """No metadata to write.
-        """
-        pass

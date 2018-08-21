07d42e4f1621201f04c2bb69fd170db4c9491ea3
Timothy Morton <tdm@astro.princeton.edu>
2018-08-21 10:57:42 +0200

    documentation for postprocess.py

--- python/lsst/qa/explorer/postprocess.py
+++ python/lsst/qa/explorer/postprocess.py
@@ -23,6 +23,31 @@ from .functors import CompositeFunctor, RAColumn, DecColumn, Column
 class PostprocessAnalysis(object):
     """Calculate columns from ParquetTable
 
+    This object manages and organizes an arbitrary set of computations
+    on a catalog.  The catalog is defined by a
+    `lsst.qa.explorer.parquetTable.ParquetTable` object (or list thereof), such as a
+    `deepCoadd_obj` dataset, and the computations are defined by a collection
+    of `lsst.qa.explorer.functor.Functor` objects (or, equivalently,
+    a `CompositeFunctor`).
+
+    After the object is initialized, accessing the `.df` attribute (which
+    holds the `pandas.DataFrame` containing the results of the calculations) triggers
+    computation of said dataframe.
+
+    One of the conveniences of using this object is the ability to define a desired common
+    filter for all functors.  This enables the same functor collection to be passed to
+    several different `PostprocessAnalysis` objects without having to change the original
+    functor collection, since the `filt` keyword argument of this object triggers an
+    overwrite of the `filt` property for all functors in the collection.
+
+    This object also allows a list of flags to be passed, and defines a set of default
+    flags that are always included even if not requested.
+
+    If a list of `ParquetTable` object is passed, rather than a single one, then the
+    calculations will be mapped over all the input catalogs.  In principle, it should
+    be straightforward to parallelize this activity, but initial tests have failed
+    (see TODO in code comments).
+
     Parameters
     ----------
     parq : `lsst.qa.explorer.ParquetTable` (or list of such)
@@ -96,18 +121,87 @@ class PostprocessAnalysis(object):
 class PostprocessConfig(Config):
     coaddName = Field(dtype=str, default="deep", doc="Name of coadd")
     functorFile = Field(dtype=str,
-                        doc='Filename of YAML functor specification',
+                        doc='Path to YAML file specifying functors to be computed',
                         default=None)
 
 
 class PostprocessTask(CmdLineTask):
     """Base class for postprocessing calculations on coadd catalogs
 
+    The purpose of this task is to perform a set of computations on
+    an input `ParquetTable` dataset (such as `deepCoadd_obj`) and write the
+    results to a new dataset (which needs to be declared in an `outputDataset`
+    attribute).
+
+    The calculations to be performed are defined in a YAML file that specifies
+    a set of functors to be computed, provided as
+    a `--functorFile` config parameter.  An example of such a YAML file
+    is the following:
+
+        funcs:
+            psfMag:
+                functor: Mag
+                args:
+                    - base_PsfFlux
+                filt: HSC-G
+                dataset: meas
+            cmodel_magDiff:
+                functor: MagDiff
+                args:
+                    - modelfit_CModel
+                    - base_PsfFlux
+                filt: HSC-G
+            gauss_magDiff:
+                functor: MagDiff
+                args:
+                    - base_GaussianFlux
+                    - base_PsfFlux
+                filt: HSC-G
+            count:
+                functor: Column
+                args:
+                    - base_InputCount_value
+                filt: HSC-G
+            deconvolved_moments:
+                functor: DeconvolvedMoments
+                filt: HSC-G
+                dataset: forced_src
+        flags:
+            - calib_psfUsed
+            - merge_measurement_i
+            - merge_measurement_r
+            - merge_measurement_z
+            - merge_measurement_y
+            - merge_measurement_g
+            - base_PixelFlags_flag_inexact_psfCenter
+            - detect_isPrimary
+
+    The names for each entry under "func" will become the names of columns in the
+    output dataset.  All the functors referenced are defined in `lsst.qa.explorer.functors`.
+    Positional arguments to be passed to each functor are in the `args` list,
+    and any additional entries for each column other than "functor" or "args" (e.g., `'filt'`,
+    `'dataset'`) are treated as keyword arguments to be passed to the functor initialization.
+
+    The "flags" entry is shortcut for a bunch of `Column` functors with the original column and
+    taken from the `'ref'` dataset.
+
+    Note, if `'filter'` is provided as part of the `dataId` when running this task (even though
+    `deepCoadd_obj` does not use `'filter'`), then this will override the `filt` kwargs
+    provided in the YAML file, and the calculations will be done in that filter.
+
+    This task uses the `lsst.qa.explorer.postprocess.PostprocessAnalysis` object
+    to organize and excecute the calculations.
+
+
     """
     _DefaultName = "Postprocess"
     ConfigClass = PostprocessConfig
 
     inputDataset = 'deepCoadd_obj'
+
+    @property
+    def outputDataset(self):
+        raise NotImplementedError('Subclass must define "outputDataset" attribute')
 
     @classmethod
     def _makeArgumentParser(cls):
@@ -133,13 +227,16 @@ class PostprocessTask(CmdLineTask):
     def doCalculations(self, parq, dataId):
         """Do postprocessing calculations
 
-        Takes a dataRef pointing to deepCoadd_obj;
+        Takes a `ParquetTable` object and dataId,
         returns a dataframe with results of postprocessing calculations.
 
         Parameters
         ----------
         parq : `lsst.qa.explorer.parquetTable.ParquetTable`
             ParquetTable from which calculations are done.
+
+        dataId : dict
+            Used to add a `patchId` column to the output dataframe.
 
         Return
         ------
@@ -163,6 +260,13 @@ class PostprocessTask(CmdLineTask):
         pass
 
 class MultibandPostprocessTask(PostprocessTask):
+    """Do the same set of postprocessing calculations on all bands
+
+    This is identical to `PostprocessTask`, except for that it does the
+    specified functor calculations for all filters present in the
+    input `deepCoadd_obj` table.  Any specific `"filt"` keywords specified
+    by the YAML file will be superceded.
+    """
 
     def doCalculations(self, parq, dataId):
         funcs = CompositeFunctor.from_yaml(self.config.functorFile)
@@ -185,6 +289,8 @@ class WriteQATableConfig(PostprocessConfig):
 class WriteQATableTask(MultibandPostprocessTask):
     """Compute columns of QA interest from coadd object tables
 
+    Only difference from MultibandPostprocessTask is the default
+    YAML functor file.
     """
     _DefaultName = "writeQATable"
     ConfigClass = WriteQATableConfig

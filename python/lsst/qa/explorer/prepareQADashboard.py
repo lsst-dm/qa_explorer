ed8ec7d9e8cfe4834614b7a4991bafb86891f03b
Timothy Morton <tmorton@ufl.edu>
2019-09-24 10:56:41 -0700

    dbg

--- python/lsst/qa/explorer/prepareQADashboard.py
+++ python/lsst/qa/explorer/prepareQADashboard.py
@@ -73,6 +73,8 @@ class TractMergeSourcesRunner(MergeSourcesRunner):
         """
         refDict = TractMergeSourcesRunner.buildRefDict(parsedCmd)
         # return [(list(ref.values()), kwargs) for ref in refDict.values()]
+        import pdb
+        pdb.set_trace()
         return [(list(p.values()), kwargs) for t in refDict.values() for p in t.values()]
 
 
